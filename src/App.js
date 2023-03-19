3a3a8e9f7a48769dbedbdd9447c13ff206de4fe6
Elvira Neziri <neziiri.elvira@gmail.com>
2023-03-19 17:58:08 +0100

    moved melas data to backend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Header/Header";
 import InfoCard from "./components/InfoCard/InfoCard";
 import Meals from "./components/Meals/Meals";
 import CartProvider from "./components/Utils/CartProvider";
-import { MEALS } from "./components/Utils/states";
+// import { MEALS } from "./components/Utils/states";
 
 
 function App() {
 
-  const [mealsInfo] = MEALS;
+  //  const [mealsInfo] = MEALS;
+  const [mealsInfo, setMealsInfo] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://food-order-87b00-default-rtdb.firebaseio.com/meals.json');
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        })
+      }
+
+      setMealsInfo(loadedMeals);
+    }
+
+    fetchMeals();
+  }, []);
 
   const [cartIsShown, setCartIsShown] = useState(false);
 
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { MEALS } from "../Utils/states";
 import style from './Melas.module.css';
 import Card from "../UI/Card";
@@ -7,21 +7,46 @@ import MealItem from "./MealItem";
 
 const Meals = (props) => {
 
-    const melasList = MEALS;
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://food-order-87b00-default-rtdb.firebaseio.com/meals.json');
+            const responseData = await response.json();
+
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price
+                })
+            }
+
+            setMeals(loadedMeals);
+        }
+
+        fetchMeals();
+    }, []);
+
+
+    const melasList = meals.map((meal) => (
+        <MealItem
+            id={meal.id}
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+    ));
 
     return (
         <section className={style.meals}>
             <Card>
                 <ul>
-                    {melasList.map((meal) =>
-                        <MealItem
-                            id={meal.id}
-                            key={meal.id}
-                            name={meal.name}
-                            description={meal.description}
-                            price={meal.price}
-                        />
-                    )}
+                    {melasList}
                 </ul>
             </Card>
         </section>
